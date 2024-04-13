4dd20c17fbe481439b60ff70f6de49565697f39f
Yuvaraj M <yuvi9607@gmail.com>
2024-04-14 00:04:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Node app!\n'
+  const msg = 'Node apps!\n'
   res.end(msg);
 });
 
